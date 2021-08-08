81eedbdbe1e1584a8664884b3ec24617b21d2dc0
cramk <unknown@unknown>
2021-08-08 11:18:29 -0400

    Rename

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto"></nav>
         <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">AnyLayer</span>
         </a>
         <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
           <ThemeSwitch />
